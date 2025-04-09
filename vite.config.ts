d015098acc4ddf232e6bf7259416cb374b5e06a6
tKwbr999 <tkwbr999@gmail.com>
2025-04-09 20:12:12 +0900

    Refactor Vite configuration for improved readability and consistency in base path handling during builds.

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
-import { defineConfig } from "vite";
-import reactSWC from "@vitejs/plugin-react-swc";
+import reactSWC from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 // https://vite.dev/config/
 export default defineConfig(({ command }) => {
   let base = '/'; // デフォルトは '/'
-  if (command === 'build') { // ビルド時のみ base を設定
+  if (command === 'build') {
+    // ビルド時のみ base を設定
     const isGithubActions = process.env.GITHUB_ACTIONS === 'true';
-    base = isGithubActions ? '/tKwbr999/' : '/'; // 環境変数で base を切り替え (リポジトリ名を修正)
+    base = isGithubActions ? '/tkwbr999/' : '/'; // 環境変数で base を切り替え (リポジトリ名を修正)
   }
 
   return {
