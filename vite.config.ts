80f80cd86563960f5b85e0eb302f7de8abeb1048
tKwbr999 <tkwbr999@gmail.com>
2025-04-10 00:36:59 +0900

    base変更

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,13 @@
 import reactSWC from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
 
-// https://vite.dev/config/
-// https://vite.dev/config/
 export default defineConfig(({ command }) => {
   let base = '/'; // デフォルトは '/'
-  if (command === 'build') {
-    // ビルド時のみ base を設定
-    const isGithubActions = process.env.GITHUB_ACTIONS === 'true';
-    base = isGithubActions ? '/tKwbr999/' : '/'; // 環境変数で base を切り替え (リポジトリ名を修正)
-  }
+  // if (command === 'build') {
+  //   // ビルド時のみ base を設定
+  //   const isGithubActions = process.env.GITHUB_ACTIONS === 'true';
+  //   base = isGithubActions ? '/tKwbr999/' : '/'; // 環境変数で base を切り替え (リポジトリ名を修正)
+  // }
 
   return {
     base: base,
